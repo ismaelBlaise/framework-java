7a76944c9ab97f5785c120fc09f04d9502bb25db
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2024-05-07 07:59:53 +0300

    Update FrontController.java

--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,7 +1,25 @@
 package controller;
 
-public class FrontController{
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet{
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+                
+        
+
+    }
 
     
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+            
+    }
     
 }
