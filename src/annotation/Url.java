75a5b681d3d9890dbe9cce2efca84f60e3e8302d
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2024-10-02 14:59:06 +0300

    ok

--- src/annotation/Url.java
+++ src/annotation/Url.java
@@ -6,6 +6,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Post {
-    
+public @interface Url {
+    String url();
 }
--- src/util/Mapping.java
+++ src/util/Mapping.java
@@ -1,23 +1,46 @@
 package util;
+import java.lang.annotation.Annotation;
 
 public class Mapping {
-    String controlleur;
-    String methode;
+    private String controlleur;
+    private String methode;
+    private Class<? extends Annotation> verb;
     
+     
+    public Mapping(String controlleur, String methode, Class<? extends Annotation> verb) {
+        this.controlleur = controlleur;
+        this.methode = methode;
+        this.verb = verb;
+    }
+
+     
     public Mapping(String controlleur, String methode) {
         this.controlleur = controlleur;
         this.methode = methode;
     }
+
+     
     public String getControlleur() {
         return controlleur;
     }
+
     public void setControlleur(String controlleur) {
         this.controlleur = controlleur;
     }
+
     public String getMethode() {
         return methode;
     }
+
     public void setMethode(String methode) {
         this.methode = methode;
+    }
+
+    public Class<? extends Annotation> getVerb() {
+        return verb;
+    }
+
+    public void setVerb(Class<? extends Annotation> verb) {
+        this.verb = verb;
     }
 }
